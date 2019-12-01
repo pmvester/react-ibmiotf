418cf2e25a460d285408726922a2533a1e90a906
Mikael Vester <mikael.vester@se.ibm.com>
2019-12-01 18:26:00 +0100

    First commit.

--- src/App.js
+++ src/App.js
@@ -1,26 +1,65 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
-function App() {
+var appConfig = require('./AppConfig.json')
+var iotfApp = require('ibmiotf').IotfApplication
+var appClient = new iotfApp(appConfig)
+
+function Pressure(props) {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <h1>{props.pressure} hPa</h1>
+  )
 }
 
-export default App;
+function Temperature(props) {
+  return (
+    <h1>{props.temperature} °C</h1>
+  )
+}
+
+export default class App extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      pressure: 1013.0,
+      temperature: 23.0
+    }
+
+    this.onMessage = this.onMessage.bind(this);
+  }
+
+  onMessage(type, id, eventType, eventFormat, payload, topic) {
+    const msg = JSON.parse(payload.toString());
+    console.log(topic);
+    console.log(msg);
+    if (topic.search('pressure') >= 0) {
+      this.setState({ pressure: msg.pressure / 100.0})
+    } else if (topic.search('temperature') >= 0) {
+      this.setState({ temperature: msg.temperature })
+    }
+  }
+
+  componentDidMount() {
+    appClient.connect()
+    appClient.on('connect', () => {
+      appClient.subscribeToDeviceEvents()
+    });
+    appClient.on('deviceEvent', this.onMessage);
+  }
+
+  componentWillUnmount() {
+    appClient.disconnect()
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <div>
+        <Pressure pressure={this.state.pressure} />
+        <Temperature temperature={this.state.temperature} />
+        </div>
+      </div>
+    );
+  }
+}
